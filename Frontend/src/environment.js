20a3434866901f488b2176e1e1af7acad5a77e81
Jitendra bisht <jitendrabisht2062004@gmail.com>
2026-03-22 21:06:49 +0530

    few changes

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,9 +1,8 @@
-let IS_PROD = false;
-
-const server =  IS_PROD ? 
+let IS_PROD = true;
+const server = IS_PROD ?
     "https://buddycall-backend-e0r6.onrender.com" :
-    `http://${window.location.hostname}:8000`
-   
+
+    "http://localhost:8000"
 
 
 export default server;
